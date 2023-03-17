cadf2fe09a53c41cba3cf6628b96168e24c62f87
Marion <mrobert148@gmail.com>
2023-03-17 14:40:11 +0100

    close cookies, click on connection and select facebook connection

--- main.py
+++ main.py
@@ -15,3 +15,26 @@ options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://tinder.com/fr")
+
+# deny cookies with XPATH
+time.sleep(5)
+print("Select the deny cookies button")
+deny_cookies_button = driver.find_element(By.XPATH, "//*[@id='q-586956664']/div/div[2]/div/div/div[1]/div[2]/button/div[2]/div[2]")
+deny_cookies_button.click()
+
+# click on connection button
+time.sleep(3)
+print("click on connection button")
+connection_button = driver.find_element(By.XPATH, "//*[@id='q-586956664']/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]")
+connection_button.click()
+
+# select facebook connection
+time.sleep(3)
+print("select facebook connection")
+facebook_button = driver.find_element(By.XPATH, "//*[@id='q1979629556']/main/div/div/div[1]/div/div/div[3]/span/div[2]/button/div[2]/div[2]")
+facebook_button.click()
+
+
+
+
+
